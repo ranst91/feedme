e56b82725461cc8de92d7b416dcb5f6dfe6c5fae
ranst91 <ran.st@hotmail.com>
2016-08-14 21:14:58 +0300

    - Added "cheese rand" switcher - Added functionality to select pizza side - Added functionality to add/delete toppings from topping array

--- feedme/js/controllers/orderCtrl.js
+++ feedme/js/controllers/orderCtrl.js
@@ -1,24 +1,42 @@
 app.controller('orderCtrl',['$scope', function ($scope) {
-    $scope.food = 'food';
-    $scope.page = ['food', 'personal', 'overview'];
-    // $scope.next = function (c,n) {
-    //     var current = '.'+c;
-    //     $('.dialog').html('<adress></adress>');
-    // }
-}]);
+    $scope.order = {
+        size: '',
+        toppings: []
+    };
+    /**
+     * Get the clicked pizza size
+     * Add a selected class
+     * Store the size selected in order's data
+     * @param $event
+     */
+    $scope.sizeSelect = function ($event) {
+        var clicked = $event.currentTarget;
+        $('.sizes').removeClass('selected');
+        $(clicked).addClass('selected');
 
-app.directive('food', function () {
-    return {
-        templateUrl: './assets/views/form/food.html'
-    }
-});
-app.directive('personal', function () {
-    return {
-        templateUrl: './assets/views/form/address.html'
-    }
-});
-app.directive('overview', function () {
-    return {
-        templateUrl: './assets/views/form/overview.html'
-    }
-});
+        this.order.size = $(clicked).data('size');
+    };
+    
+    $scope.toppings = [
+        {name: 'Tomato Sauce', letter: 'X'},
+        {name: 'Mozarella Cheese', letter: 'C'},
+        {name: 'Mushrooms', letter: 'M'},
+        {name: 'Tomatos', letter: 'T'},
+        {name: 'Pepperoni', letter: 'P'},
+        {name: 'Schinken', letter: 'H'}
+    ];
+    
+    $scope.selectIngredients = function ($event) {
+        var selected = $( "select option:selected" );
+        this.order.toppings.push($(selected).val());
+        $('.selected_opt').append('<li data-val="'+$(selected).val()+'"><span>'+$(selected).text()+'</span><span class="delete"></span></li>');
+    };
+    
+    $(document).on('click', '.delete', function (e) {
+        var clicked = $(e.currentTarget).parent();
+        var i = $scope.order.toppings.indexOf(clicked.data('val'));
+        $scope.order.toppings.splice(i, 1);
+        clicked.remove();
+    });
+    $("[name='cheese-rand']").bootstrapSwitch();
+}]);
